1ae14fb568cd5996ccf71f2659a5b2a98b9e5662
SnowScapes <siwon2113@gmail.com>
2024-04-29 11:40:56 +0900

    First commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SpartaDungeon_Team_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
