a9195f588410dc11242dc90a07340b510d8cddf0
SnowScapes <siwon2113@gmail.com>
2024-04-29 19:29:31 +0900

    add Equipment struct

---
+++ Equipment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaDungeon_Team_
+{
+    enum EquipmentType
+    {
+        Armor = 0,
+        Weapon = 1
+    }
+    struct Equipment
+    {
+        public string Name; // 장비 이름
+        public EquipmentType Type; // 장비 타입(방어구, 무기)
+        public int Stat; // 장비 스탯
+        public string Description; // 장비 설명
+        public int Price; // 장비 가격
+        public bool Purchased; // 구매 여부
+    }
+}
