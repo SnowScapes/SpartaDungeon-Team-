127a6ce9de459bb64331eb52b02f83a89c6c01d7
choi <rhksgh1993@naver.com>
2024-05-01 21:07:22 +0900

    11

--- Program.cs
+++ Program.cs
@@ -1,44 +1,36 @@
-﻿namespace SpartaDungeon_Team_
+namespace SpartaDungeon_Team_
 {
     internal class Program
     {
         public static Player PlayerData;
-
+        
         static void Main(string[] args)
         {
-
             // 필요한 클래스들 불러오기
             SaveLoad saveLoad = new SaveLoad();
+            Intro introScreen = new Intro();
             MainScreen mainScreen = new MainScreen();
             Shop shopScreen = new Shop();
             Inventory inventoryScreen = new Inventory();
-
-            Stage stageScreen = new Stage();
-
+            Status statusScreen = new Status();
             Battle battle = new Battle();
 
-
-            // 플레이어정보(PlayerInfo.Json) 로드
-            // 아이템정보(ItemInfo.Json) 로드
-            saveLoad.LoadPlayerInfo();
-            saveLoad.LoadItemInfo();
+            //인트로 화면 로드
+            //introScreen.IntroScreen();
 
             // 행동 선택하기
-            while (true)
+            while(true)
             {
                 //메인화면 출력 및 메뉴 선택
                 switch (mainScreen.ShowMain())
                 {
-                    case Screen.Status: break; // 상태 보기 화면 메서드 불러오기
-                    case Screen.Dungeon: stageScreen.MainStage(); break; // 전투 시작 화면 메서드 불러오기
-                    case Screen.Inventory: inventoryScreen.ShowInventory(); break; // 인벤토리 화면 메서드 불러오기
+                    case Screen.Status: statusScreen.StatusMenu(); break; // 상태 보기 화면 메서드 불러오기
+                    case Screen.Dungeon: battle.BattleEntering(); break; // 전투 시작 화면 메서드 불러오기
+                    case Screen.Inventory:inventoryScreen.ShowInventory(); break; // 인벤토리 화면 메서드 불러오기
                     case Screen.Shop: shopScreen.ViewShop(); break; // 상점 화면 메서드 불러오기
                     case Screen.Save: saveLoad.SavePlayerInfo(); Console.WriteLine("저장이 완료되었습니다."); Thread.Sleep(1000); break; // 저장 메소드 불러오기
                 }
             }
-
         }
     }
-
-
 }
--- SaveLoad.cs
+++ SaveLoad.cs
@@ -1,59 +1,83 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace SpartaDungeon_Team_
 {
     internal class SaveLoad
     {
-        string playerInfoPath = "PlayerInfo.Json";
+        
         string itemInfoPath = "ItemInfo.Json";
-        public void LoadPlayerInfo()
+        public bool LoadPlayerInfo()
         {
-            bool fileExist = File.Exists(playerInfoPath);
-
-            if (fileExist)
+            string playerInfoPath = "PlayerInfo.Json";
+            while (true)
             {
-                // PlayerInfo.Json 파일이 존재할 경우 캐릭터 정보 불러오기
-                string jsonText = File.ReadAllText(playerInfoPath);
+                Console.Clear();
+                Console.WriteLine(".____                       .___      ________                           ");
+                Console.WriteLine("|    |     ____ _____     __| _/     /  _____/ _____     _____    ____   ");
+                Console.WriteLine("|    |    /  _ \\\\__  \\   / __ |     /   \\  ___ \\__  \\   /     \\ _/ __ \\  ");
+                Console.WriteLine("|    |___(  <_> )/ __ \\_/ /_/ |     \\    \\_\\  \\ / __ \\_|  Y Y  \\\\  ___/  ");
+                Console.WriteLine("|_______ \\\\____/(____  /\\____ |      \\______  /(____  /|__|_|  / \\___  > ");
+                Console.WriteLine("        \\/           \\/      \\/             \\/      \\/       \\/      \\/  ");
+                Console.WriteLine("1. 1번 데이터 불러오기");
+                Console.WriteLine("2. 2번 데이터 불러오기");
+                Console.WriteLine("3. 3번 데이터 불러오기");
+                Console.WriteLine("4. 돌아가기");
 
-                JObject jsonObject = JObject.Parse(jsonText);
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.D1: playerInfoPath = "Slot1PlayerInfo.Json"; break;
+                    case ConsoleKey.D2: playerInfoPath = "Slot2PlayerInfo.Json"; break;
+                    case ConsoleKey.D3: playerInfoPath = "Slot3PlayerInfo.Json"; break;
+                    case ConsoleKey.D4: return false;
+                }
+                
+                if (File.Exists(playerInfoPath))
+                {
+                    // PlayerInfo.Json 파일이 존재할 경우 캐릭터 정보 불러오기
+                    string jsonText = File.ReadAllText(playerInfoPath);
 
-                Program.PlayerData.Name = (string)jsonObject["name"];
-                Program.PlayerData.Level = (int)jsonObject["level"];
-                Program.PlayerData.Attack = (float)jsonObject["attack"];
-                Program.PlayerData.Defense = (float)jsonObject["defense"];
-                Program.PlayerData.Health = (int)jsonObject["health"];
-                Program.PlayerData.Gold = (int)jsonObject["gold"];
-                Program.PlayerData.Potion = (int)jsonObject["potion"];
-                Stage.SetStageLevel((int)jsonObject["dungenLevel"]);
-            }
-            else
-            {
-                // PlayerInfo.Json 파일이 없을 경우 새로운 캐릭터 생성
-                NewPlayer createNewPlayer = new NewPlayer();
-                createNewPlayer.CreatePlayer();
+                    JObject jsonObject = JObject.Parse(jsonText);
+
+                    Program.PlayerData.Name = (string)jsonObject["name"];
+                    Program.PlayerData.Job = (Jobs)(int)jsonObject["job"];
+                    Program.PlayerData.Level = (int)jsonObject["level"];
+                    Program.PlayerData.Attack = (float)jsonObject["attack"];
+                    Program.PlayerData.Defense = (float)jsonObject["defense"];
+                    Program.PlayerData.Health = (int)jsonObject["health"];
+                    Program.PlayerData.Gold = (int)jsonObject["gold"];
+                    Program.PlayerData.Potion = (int)jsonObject["potion"];
+                    LoadItemInfo();
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("\n저장 데이터가 존재하지 않습니다.");
+                    Thread.Sleep(1000);
+                }
             }
         }
 
         public void SavePlayerInfo()
         {
+            /*
             JObject playerData = new JObject(
                 new JProperty("name", Program.PlayerData.Name),
+                new JProperty("job", Program.PlayerData.Job),
                 new JProperty("level", Program.PlayerData.Level),
                 new JProperty("attack", Program.PlayerData.Attack),
                 new JProperty("defense", Program.PlayerData.Defense),
                 new JProperty("health", Program.PlayerData.Health),
                 new JProperty("gold", Program.PlayerData.Gold),
-                new JProperty("potion", Program.PlayerData.Potion),
-                new JProperty("dungenLevel", Stage.GetStageLevel())
+                new JProperty("potion", Program.PlayerData.Potion)
             );
 
-            File.WriteAllText(playerInfoPath, playerData.ToString());
+            File.WriteAllText(playerInfoPath, playerData.ToString());*/
         }
 
         public void LoadItemInfo()
