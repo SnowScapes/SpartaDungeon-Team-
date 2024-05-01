29e5bfff0ba2c196e666b71f4f80fe66f2d1a4f5
happykud <kyhaa71455@gmail.com>
2024-05-01 17:18:58 +0900

    Buttle2

--- Monster.cs
+++ Monster.cs
@@ -16,6 +16,7 @@ namespace SpartaDungeon_Team_
         public int atk; // 공격력
         public int evasion; // 회피율
         public int accuracy; // 명중률
+        public bool isDeath;
     }
 
 
@@ -23,12 +24,12 @@ namespace SpartaDungeon_Team_
     {
         List<Monster> monsters = new List<Monster>
         {
-            new Monster { index = 1 , level = 2, name = "미니언", hp = 15, mp = 15, atk = 5, evasion = 10, accuracy = 10 },
-            new Monster { index = 2 , level = 3, name = "공허충", hp = 10, mp = 10, atk = 9, evasion = 12, accuracy = 12 },
-            new Monster { index = 3 , level = 5, name = "대포미니언", hp = 25, mp = 25, atk = 8, evasion = 18, accuracy = 18 }
+            new Monster {index = 0 , level = 2, name = "미니언", hp = 15, mp = 15, atk = 5, evasion = 10, accuracy = 10 , isDeath = false},
+            new Monster {index = 1 , level = 3, name = "공허충", hp = 10, mp = 10, atk = 9, evasion = 12, accuracy = 12 , isDeath = false},
+            new Monster {index = 2 , level = 5, name = "대포미니언", hp = 25, mp = 25, atk = 8, evasion = 18, accuracy = 18 , isDeath = false}
         };
 
-        public Monster getMonsterInfo(int index)
+        public Monster GetMonsterInfo(int index)
         {
             return monsters[index];
         }
