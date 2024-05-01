68ebc42e8ca4bf0a08e11de4ec0148c286e28d61
pjm0720 <woals720@naver.com>
2024-05-01 20:27:29 +0900

    상태창 ,레벨업 구현
    상태창, 레벨 업 기능 (임시) 구현

--- Player.cs
+++ Player.cs
@@ -8,6 +8,7 @@ namespace SpartaDungeon_Team_
 {
     struct Player
     {
+        
         public int Level; // 플레이어 레벨
         public string Name; // 플레이어 이름
         public float LevelUpAtk;    // 레벨 업 시 증가할 공격력
@@ -20,6 +21,9 @@ namespace SpartaDungeon_Team_
         public Equipment Armor; // 현재 장착 중 방어구
         public Equipment Weapon; // 현재 장착 중 무기
         public int Exp;     // 현재 내 경험치
+        public int GainExp;   // 얻은 경험치
+        public int RequireExp;   // 다음레벨까지의 경험치
+        public int MaxLevel;    // 최고 레벨
 
         //초기 플레이어 스탯 설정
         public void SetPlayerStat()
@@ -35,14 +39,53 @@ namespace SpartaDungeon_Team_
 
         public float TotalAtk() // 총 공격력 = 공격력 + 장착 장비 공격력
         {
-            return Program.PlayerData.Attack + Weapon.value;
+            return Program.PlayerData.Attack + Program.PlayerData.Weapon.Stat;
         }
 
         public float TotalDef()  // 총 방어력 = 방어력 + 장착 장비 방어력
         {
-            return Program.PlayerData.Defense + Armor.value;
+            return Program.PlayerData.Defense + Program.PlayerData.Armor.Stat;
         }
 
+        public void GetExp()    // 경험치 얻기
+        {
+            //Program.PlayerData.GainExp = rewardExp;   // 얻은 경험치 == 잡은 몬스터의 레벨
+            //Program.PlayerData.Exp += Program.PlayerData.GainExp; // 현재 경험치에 얻은 경험치 더해주기
+            Program.PlayerData.GainExp = 15;
+            Program.PlayerData.Exp += Program.PlayerData.GainExp;
+            Program.PlayerData.MaxLevel = 5;
+            if (Program.PlayerData.Level >= Program.PlayerData.MaxLevel)   // 최고 레벨 도달 시
+            {
+                // 현재 경험치 더 이상 안오르게 하기
+                Program.PlayerData.Exp = 0;
+            }
+        }
+
+        public void CheckLevelUp()    //레벨 업 조건 확인
+        {
+           switch (Program.PlayerData.Level)
+            {
+                case 1:
+                    Program.PlayerData.RequireExp = 10;    // 레벨 1 일때 다음 레벨 필요 경험치 10
+                    break;
+                case 2:
+                    Program.PlayerData.RequireExp = 35;    // 레벨 2 일때 다음 레벨 필요 경험치 35
+                    break;
+                case 3:
+                    Program.PlayerData.RequireExp = 65;    // 레벨 3 일때 다음 레벨 필요 경험치 65
+                    break;
+                case 4:
+                    Program.PlayerData.RequireExp = 100;   // 레벨 4 일때 다음 레벨 필요 경험치 100
+                    break;
+            }
+
+            if (Program.PlayerData.Exp >= Program.PlayerData.RequireExp)
+            {
+                LevelUp();
+                int remainExp = Program.PlayerData.Exp - Program.PlayerData.RequireExp;  
+                Program.PlayerData.Exp = remainExp;              
+            }
+        }
         public void LevelUp()   // 플레이어 레벨 업
         {
             float LevelUpAtk = 0.5f;
@@ -51,7 +94,9 @@ namespace SpartaDungeon_Team_
             Program.PlayerData.Attack += LevelUpAtk;
             Program.PlayerData.Defense += LevelUpDef;
 
-            Console.WriteLine($"레벨 업! Lv. {Program.PlayerData.Level - 1} -> Lv. {Program.PlayerData.Level}    공격력 : {Program.PlayerData.TotalAtk()} (+ 0.5)   방어력 : {Program.PlayerData.TotalDef()} (+ 1)");
+            Console.WriteLine($"레벨 업! Lv. {Program.PlayerData.Level - 1} -> Lv. {Program.PlayerData.Level}");
+            Console.WriteLine($"공격력 : {Program.PlayerData.TotalAtk()} (+ 0.5)");
+            Console.WriteLine($"방어력 : {Program.PlayerData.TotalDef()} (+ 1)");
         }
         //아이템 장착, 해제 기능
         //type(방어구or무기)에 따라 각 위치에 장착
--- Status.cs
+++ Status.cs
@@ -8,34 +8,57 @@ namespace SpartaDungeon_Team_
 {
     internal class Status
     {
+        MainScreen MainScreen = new MainScreen();
         bool NotValid = false;
+
+        public int PromptMenuChoice(int min, int max)
+        {
+            while (true)
+            {
+                Console.Write("원하시는 행동을 입력해주세요: ");
+                if (int.TryParse(Console.ReadLine(), out int choice) && choice >= min && choice <= max)
+                {
+                    return choice;
+                }
+                Console.WriteLine("잘못된 입력입니다. 다시 입력해주세요.");
+            }
+        }
         public void StatusMenu()  //플레이어 상태창 
         {
-            notValid:      // 레이블 정의   프로그램의 실행 흐름을 제어하기 위해 사용
             Console.Clear();
+            Player player = new Player();
+            player.GetExp();
+            player.CheckLevelUp();
             Console.WriteLine("상태 보기");
             Console.WriteLine("캐릭터의 정보가 표시됩니다.");
             Console.WriteLine();
-            Console.WriteLine("Lv. " + Program.PlayerData.Level.ToString("00"));
+            Console.WriteLine("Lv. {0}", Program.PlayerData.Level >= Program.PlayerData.MaxLevel ? $"{Program.PlayerData.Level.ToString("00")} [최고 레벨]" : Program.PlayerData.Level.ToString("00"));
             Console.WriteLine("{0} (전사)", Program.PlayerData.Name);
-            Console.WriteLine("공격력 : {0}{1}", Program.PlayerData.TotalAtk().ToString(), Program.PlayerData.Weapon.Name != null ? string.Format(" (+{0})", Program.PlayerData.Weapon.value) : "");
-            Console.WriteLine("방어력 : {0}{1}", Program.PlayerData.TotalDef().ToString(), Program.PlayerData.Armor.Name != null ? string.Format(" (+{0})", Program.PlayerData.Armor.value) : "");
+            Console.WriteLine("공격력 : {0}{1}", Program.PlayerData.TotalAtk().ToString(), Program.PlayerData.Weapon.Name != null ? string.Format(" (+{0})", Program.PlayerData.Weapon.Stat) : "");
+            Console.WriteLine("방어력 : {0}{1}", Program.PlayerData.TotalDef().ToString(), Program.PlayerData.Armor.Name != null ? string.Format(" (+{0})", Program.PlayerData.Armor.Stat) : "");
             Console.WriteLine("체  력 : " + Program.PlayerData.Health.ToString());
             Console.WriteLine("Gold : {0} G", Program.PlayerData.Gold.ToString());
             Console.WriteLine("Exp : {0}", Program.PlayerData.Exp.ToString());
+            Console.WriteLine("다음 레벨 업까지 필요한 Exp : {0}", Program.PlayerData.Level >= Program.PlayerData.MaxLevel ? "0" : (Program.PlayerData.RequireExp - Program.PlayerData.Exp).ToString("00"));
             Console.WriteLine();
             Console.WriteLine("0. 나가기");
-            if (NotValid)     //잘못된 입력일 때 잘못된 입력 출력 및 화면 재출력
+            //if (NotValid)     //잘못된 입력일 때 잘못된 입력 출력 및 화면 재출력
+            //{
+            //    Console.WriteLine("잘못된 입력입니다.");
+            //    NotValid = false;
+            //}
+            //Console.WriteLine("원하시는 행동을 입력해주세요 : ");
+            //Console.Write(">>");
+            //if (Console.ReadLine() != "0")
+            //{
+            //    NotValid = true;
+            //    goto notValid;     //notValid 레이블로 이동시키기
+            //}
+           switch (PromptMenuChoice(0, 0))
             {
-                Console.WriteLine("잘못된 입력입니다.");
-                NotValid = false;
-            }
-            Console.WriteLine("원하시는 행동을 입력해주세요 : ");
-            Console.Write(">>");
-            if (Console.ReadLine() != "0")
-            {
-                NotValid = true;
-                goto notValid;     //notValid 레이블로 이동시키기
+                case 0:
+                    MainScreen.ShowMain();
+                    break;
             }
         }
     }
